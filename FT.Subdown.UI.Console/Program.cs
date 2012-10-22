84456ab13b5e2e99fecb0e2986fd7b24bd68d0a0
Antoine Jaussoin <a.jaussoin@laposte.net>
2012-10-22 22:15:30 +0100

    First commit

---
+++ FT.Subdown.UI.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FT.Subdown.UI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.Out.WriteLine("End");
+            System.Console.Read();
+        }
+    }
+}
